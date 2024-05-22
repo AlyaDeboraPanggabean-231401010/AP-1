f0d2619a884c1c26e0a23188fbc2f62282b9d984
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 21:09:32 +0700

    bmi.cpp

--- pertemuan 3/bmi.cpp
+++ pertemuan 3/bmi.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
-#include <iomanip>
+#include <iomanip> // ini dipakai buat fungsi setprecision
 
 using namespace std;
 
 int main()
 {
+    // Program BMI
     float berat, tinggi, bmi;
 
-        cout<<"Masukkan berat badan anda (kg) : ";
-        cin>>berat;
+        cout << "Masukkan berat badan anda (kg) : ";
+        cin >> berat;
 
-        cout<<"Masukkan tinggi anda (m) : ";
-        cin>>tinggi;
+        cout << "Masukkan tinggi anda (m) : ";
+        cin >> tinggi;
 
         bmi = berat/(tinggi*tinggi);
         
-        cout<<fixed<<setprecision(0);
-        cout<<"BMI Anda : "<<bmi<<endl;
+        cout << fixed << setprecision(0); // bisa kita pakai untuk berapa angka di belakang koma
+        cout << "BMI Anda : " << bmi << endl;
 
-        if (bmi < 18.5){
-            cout<<"Kategori berat badan : kurang "<<endl;
+        if (bmi < 18.5) {
+            cout << "Kategori berat badan : kurang " << endl;
         }
 
         if (bmi >= 18.5 && bmi < 25) {
-        cout<<"Kategori berat badan : normal "<<endl;
+        cout << "Kategori berat badan : normal " << endl;
         }
 
-        else if (bmi>=25 && bmi<30){
-             cout<<"Kategori berat badan : berlebih "<<endl;
+        else if (bmi>=25 && bmi<30) {
+             cout << "Kategori berat badan : berlebih " << endl;
         }
 
-        else if ( bmi >= 30){
-            cout<<"Kategori berat badan : obesitas "<<endl;
+        else if ( bmi >= 30) {
+            cout << "Kategori berat badan : obesitas " << endl;
         }
 
     return 0;
