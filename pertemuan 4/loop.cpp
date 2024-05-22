42eda32d48dfce6845dd079377c6c5d4eec6e7e5
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 21:52:20 +0700

    Add files via upload

---
+++ pertemuan 4/loop.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    //goto label
+    // a:
+    //     cout<<"Hello world"<<endl;
+    //     goto d;
+    // b:
+    //     cout<<"Universitas Sumatera Utara"<<endl;
+    //     return 0;
+    // c:
+    //     cout<<"Fasilkom-TI"<<endl;
+    //     goto b;
+    // d:
+    //     cout<<"Ilmu Komputer"<<endl;
+    //     goto c;
+    
+    //tampilin bilangan genap pake goto
+    // int i = 1;
+    // genap:
+    //     if(i%2 == 0){
+    //         cout<< i << " ";
+    //     }
+    //     i++;
+
+    // if(i<=10){
+    //     goto genap;
+    // }
+
+    //while
+    // int i = 1;
+    // while (i <= 10){
+    //     if(i % 2 == 0){
+    //         cout << i << " ";
+    //     }
+    //     i++;
+    // }
+
+    //do while
+    // int i = 1;
+    // do {
+    //     cout<<i<<endl;
+    // }
+    // while (i<=0);
+
+    //for
+    // for (int i=1; i<=10; i+=2){
+    //     cout<<"Hello world"<<endl;
+    // }
+
+    // for (int i = 1; i<=5; i++){
+    //     for (int j = 1; j<=5; j++){
+    //     cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //buat segitiga siku-siku
+    // int i,j; 
+    //     for(i=1; i<=5; i++)
+    //     {
+    //         for(j=1; j<=i; j++)
+    //         {
+    //             cout<<"* ";
+    //         }
+    //     cout<<endl;
+    //     } 
+}
