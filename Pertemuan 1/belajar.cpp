e8675af82590d101d435881d15d5f8369826eb88
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 20:17:36 +0700

    belajar.cpp

--- Pertemuan 1/belajar.cpp
+++ Pertemuan 1/belajar.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> // header untuk c++
 using namespace std;
 
 main()
@@ -8,23 +8,23 @@ main()
     char kom;
     float ip;
     
-    cout<<"Hello world" <<endl;
+    cout << "Hello world" << endl; 
 
-    cout<<"Masukkan nama : ";
-    // cin>>nama; 
+    cout << "Masukkan nama : ";
+    // cin >> nama; 
     getline (cin, nama);
 
-    cout<<"Masukkan nim : ";
-    cin>>nim; 
+    cout << "Masukkan nim : ";
+    cin >> nim; 
 
-    cout<<"Masukkan kom : ";
-    cin>>kom; 
+    cout << "Masukkan kom : ";
+    cin >> kom; 
 
-    cout<<"Masukkan ip : ";
-    cin>>ip; 
+    cout << "Masukkan ip : ";
+    cin >> ip; 
 
-    cout<<"Nama : "<<nama<<endl;
-    cout<<"NIM : "<<nim<<endl;
-    cout<<"KOM : "<<kom<<endl;
-    cout<<"IP : "<<ip<<endl;
+    cout << "Nama : " << nama << endl;
+    cout << "NIM : "<< nim << endl;
+    cout << "KOM : " << kom << endl;
+    cout << "IP : " << ip <<endl;
 }
