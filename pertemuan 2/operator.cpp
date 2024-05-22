de9fff6b70294610d815d3298f1b3d8fd2f69035
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 20:54:22 +0700

    operator.cpp

--- pertemuan 2/operator.cpp
+++ pertemuan 2/operator.cpp
@@ -5,93 +5,96 @@ int main()
     {
         int a,b;
 
-        //assignment op
+        //assignment op, ini digunakan untuk memberi nilai ke dalam suatu variabel
         a = 3;
         b = 5;
 
-        // arithmetical op
+        // arithmetical op, ini digunakan untuk perhitungan matematika
         // int tambah = a + b;
         // int kurang = a - b;
         // int kali = a * b; 
         // float bagi = a / b; // (float ) a / (float) b
         // int modulo = a % b;
 
-        // cout<<"Hasil penjumlahan = "<<tambah<<endl;
-        // cout<<"Hasil pengurangan = "<<kurang<<endl;
-        // cout<<"Hasil perkalian = "<<kali<<endl;
-        // cout<<"Hasil pembagian = "<<bagi<<endl;
-        // cout<<"Hasil sisa bagi = "<<modulo<<endl;
+        // cout << "Hasil penjumlahan = " << tambah << endl;
+        // cout << "Hasil pengurangan = " << kurang << endl;
+        // cout << "Hasil perkalian = " << kali << endl;
+        // cout << "Hasil pembagian = " << bagi << endl;
+        // cout << "Hasil sisa bagi = " << modulo << endl;
 
-        //relational op
-        // cout<< (a == b)<<endl;
-        // cout<< (a>b)<<endl;
-        // cout<< (a >= b)<<endl;
-        // cout<< (a < b)<<endl;
-        // cout<< (a <= b)<<endl;
-        // cout<< (a != b)<<endl;
+        // relational op, ini digunakan untuk membandingkan nilai dari dua operand, nilainya True atau False
+        // cout << (a == b) << endl;
+        // cout << (a > b) << endl;
+        // cout << (a >= b) << endl;
+        // cout << (a < b) << endl;
+        // cout << (a <= b) << endl;
+        // cout << (a != b) << endl;
 
-        //logical op
-        // cout<< (true && true)<<endl;
-        // cout<< (true && false)<<endl;
-        // cout<< (false && true)<<endl;
-        // cout<< (false && false)<<endl;
+        // logical op, ini digunakan untuk membandingkan dua nilai variabel bertipe boolean
+        // Logika AND
+        // cout << (true && true) << endl; 
+        // cout << (true && false) << endl;
+        // cout << (false && true) << endl;
+        // cout << (false && false) << endl;
 
-        // cout<< (true || true)<<endl;
-        // cout<< (true || false)<<endl;
-        // cout<< (false || true)<<endl;
-        // cout<< (false || false)<<endl;
-
-        // cout<< !true <<endl;
-        // cout<< !false<<endl;
+        // Logika OR
+        // cout << (true || true) << endl;
+        // cout << (true || false) << endl;
+        // cout << (false || true) << endl;
+        // cout << (false || false) << endl;
         
-        //Bitwise op
-        // cout<< (5 & 7)<<endl;
-        // cout<< (5 | 7)<<endl;
-        // cout<< (5 ^ 7)<<endl;
-        // cout<< (~7)<<endl;
-        // cout<< (7 << 2)<<endl;
-        // cout<< (7 >> 2)<<endl;
+        // Logika NOT
+        // cout << !true << endl;
+        // cout << !false << endl;
+        
+        // Bitwise op, digunakan untuk operasi per bit pada nilai Integer
+        // cout << (5 & 7) << endl; // AND
+        // cout << (5 | 7) << endl; // OR
+        // cout << (5 ^ 7) << endl; // XOR
+        // cout << (~7) << endl; // NOT
+        // cout << (7 << 2) << endl; // SHL
+        // cout << (7 >> 2 )<< endl; // SHR
 
         //shorthand
         // a += 7; // krna a di atas 3 mau kita buat 10
-        // cout<<a<<endl;
+        // cout << a << endl;
 
-        // a -= 7; 
-        // cout<<a<<endl;
+        // a -= 7; // sama artinya, a = a - 7 
+        // cout << a << endl;
 
         // a *= 7; 
-        // cout<<a<<endl;
+        // cout << a << endl;
 
         // a /= 7; 
-        // cout<<a<<endl;
+        // cout << a << endl;
 
-        //increment & decrement
-        //pre incre
+        // increment & decrement
+        // pre incre, yaitu proses penambahan langsung dilakuin, jadi ketika nilai a = 3, maka akan menjadi 4
         // ++a;
-        // cout<<a<<endl;
+        // cout << a << endl;
 
         // ++b;
-        // cout<<b<<endl;
+        // cout << b << endl;
 
-        // //post incre
+        // post incre, kebalikan dari yang pre incre, penambahannya gak langsung dilakuin
         // a++;
-        // cout<<a<<endl;
+        // cout << a << endl;
 
         // b++;
-        // cout<<b<<endl;
+        // cout << b << endl;
 
-        //pre decre
-        // cout<< a <<endl;
-        // cout<< --a <<endl;
+        // pre decre,  yaitu proses pengurangan langsung dilakuin
+        // cout << a << endl;
+        // cout << --a << endl;
 
         // cout<< b << endl;
         // cout<< --b <<endl;
 
-        //post decre
-        // cout<< a-- <<endl;
-        // cout<< a <<endl;
+        // post decre, proses pengurangannya gak langsung dilakuin
+        // cout << a-- << endl;
+        // cout << a << endl;
 
-        // cout<< b-- << endl;
-        // cout<< b <<endl;
+        // cout << b-- << endl;
+        // cout << b << endl;
 
     }
