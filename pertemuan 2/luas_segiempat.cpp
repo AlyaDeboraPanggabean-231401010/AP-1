e99356d3df1bc87aa53a39da6450a9455a1e157e
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 20:21:45 +0700

    luas_segiempat.cpp

--- pertemuan 2/luas_segiempat.cpp
+++ pertemuan 2/luas_segiempat.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main()
     {
         // int p, l;
-        float luas, p, l;
+        float luas, p, l; // lebih cocok menggunakan float
 
-        cout<<"masukkan panjang : ";
-        cin>>p;
+        cout << "masukkan panjang : ";
+        cin >> p;
 
-        cout<<"masukkan lebar : ";
-        cin>>l;
+        cout << "masukkan lebar : ";
+        cin >> l;
 
         luas = p * l;
-        cout<<"Luas = "<<luas<<endl;
+        cout << "Luas = " << luas << endl;
 
         return 0;
 
