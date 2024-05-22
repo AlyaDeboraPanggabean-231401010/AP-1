96496b010f0df33b71cffa37a3c4c23036fdd44a
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 20:19:01 +0700

    belajar C.cpp

--- Pertemuan 1/belajar C.cpp
+++ Pertemuan 1/belajar C.cpp
@@ -1,35 +1,34 @@
 #include <stdio.h> // header bawaan C
 #include <concio>
 
-int main ()
-{
-// int nim;
-// char kom;
-// char nama[20];
-// float ip;
+int main () {
+int nim;
+char kom;
+char nama[20];
+float ip;
 
-//     printf("Hello World\n");
+    printf("Hello World\n");
 
-//     printf("Masukkan nama : ");
-//     // scanf(" %[^\n]", &nama); // terlalu ribet
-//     gets(nama); //get string, bisa pakai ini 
+    printf("Masukkan nama : ");
+    // scanf(" %[^\n]", &nama); // terlalu ribet
+    gets(nama); //get string, bisa pakai ini 
 
-//     printf("Masukkan nim : ");
-//     scanf(" %d", &nim);
+    printf("Masukkan nim : ");
+    scanf(" %d", &nim);
 
-//     printf("Masukkan KOM : ");
-//     scanf(" %c", &kom);
+    printf("Masukkan KOM : ");
+    scanf(" %c", &kom);
 
-//     printf("Masukkan IP : ");
-//     scanf("%f", &ip);
+    printf("Masukkan IP : ");
+    scanf("%f", &ip);
 
-//     // printf("Nama: %s\n", nama); biasanya pake yg dibwah
-//     printf("Nama : ");
-//     puts(nama);
-//     
-//     printf("NIM: %d\n", nim);
-//     printf("KOM: %c\n", kom);
-//     printf("IP: %.2f\n", ip);
+    // printf("Nama: %s\n", nama); biasanya pake yg dibwah
+    printf("Nama : ");
+    puts(nama);
+    
+    printf("NIM: %d\n", nim);
+    printf("KOM: %c\n", kom);
+    printf("IP: %.2f\n", ip);
 
     printf("press any button to continue...");
     getch();
