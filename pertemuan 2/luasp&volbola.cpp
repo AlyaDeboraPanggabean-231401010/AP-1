8d32744ab59047e47e4f933c3d95f710f58da45d
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 20:25:41 +0700

    luasp&volbola.cpp

--- pertemuan 2/luasp&volbola.cpp
+++ pertemuan 2/luasp&volbola.cpp
@@ -3,17 +3,16 @@ using namespace std;
 
 int main()
     {
+        // Program untuk menentukan volume dan luas sebuah bola
         float luas, volume, jari;
-        const float phi = 3.14;
+        const float phi = 3.14; // konstanta 
 
-        cout<<"Input jari-jari : ";
-        cin>>jari;
+        cout << "Input jari-jari : ";
+        cin >> jari;
 
         volume = (4/3) * (phi) * (jari*jari*jari);
         luas = 4 * phi * jari * jari;
 
-         cout<<"Volume : "<<volume<<endl;
-         cout<<"luas : "<<luas<<endl;
-         
-
+        cout << "Volume : " << volume << endl;
+        cout << "luas : " << luas << endl;
     }
