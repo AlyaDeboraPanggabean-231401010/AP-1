d7a6096e89bb7d96bc360ab9026d644bd7aa8eba
Alya Debora Panggabean <151598735+AlyaDeboraPanggabean-231401010@users.noreply.github.com>
2024-05-22 21:00:16 +0700

    belajar C.cpp

--- Pertemuan 1/belajar C.cpp
+++ Pertemuan 1/belajar C.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h> // header bawaan C
+#include <stdio.h> // header C
 #include <concio>
 
 int main () {
@@ -7,14 +7,14 @@ char kom;
 char nama[20];
 float ip;
 
-    printf("Hello World\n");
+    printf("Hello World\n"); // fungsi keluaran pada C
 
     printf("Masukkan nama : ");
     // scanf(" %[^\n]", &nama); // terlalu ribet
-    gets(nama); //get string, bisa pakai ini 
+    gets(nama); // get string, bisa pakai ini 
 
     printf("Masukkan nim : ");
-    scanf(" %d", &nim);
+    scanf(" %d", &nim); // fungsi masukan pada C
 
     printf("Masukkan KOM : ");
     scanf(" %c", &kom);
@@ -24,12 +24,12 @@ float ip;
 
     // printf("Nama: %s\n", nama); biasanya pake yg dibwah
     printf("Nama : ");
-    puts(nama);
+    puts(nama); // put string, digunakan untuk string dalam bentuk array char
     
     printf("NIM: %d\n", nim);
     printf("KOM: %c\n", kom);
     printf("IP: %.2f\n", ip);
 
     printf("press any button to continue...");
-    getch();
+    getch(); // gak nampilin karakter yang kita input, cuman di simpen di memori
 }
